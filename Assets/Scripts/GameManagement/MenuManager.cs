765c8f0bfca38d8e591f62025240e085e7f20017
JKaraky <jawad.alkaraky@gmail.com>
2024-01-28 13:30:58 +0200

    Main menu

--- Assets/Scripts/GameManagement/MenuManager.cs
+++ Assets/Scripts/GameManagement/MenuManager.cs
@@ -5,8 +5,25 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
+    [SerializeField] private GameObject learnMenu;
+
     public void StartGame()
     {
         SceneManager.LoadScene("LevelOne", LoadSceneMode.Single);
+    }
+
+    public void GoToLearn()
+    {
+        learnMenu.SetActive(true);
+    }
+
+    public void GoBackFromLearn()
+    {
+        learnMenu.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
